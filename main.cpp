7973581958349e0dba0c66c34f11ceed3ac71bf4
RomanPylypivKI <62753409+RomanPylypivKI@users.noreply.github.com>
2020-05-21 16:20:17 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include "matrix.h"
+using namespace std;
+int main(void)
+{
+	int row, col;
+	cout << "Enter row: ";
+	cin >> row;
+	cout << "Enter col: ";
+	cin >> col;
+	Matrix<int> m(row, col);
+	cout << m;
+	m.count_res();
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
